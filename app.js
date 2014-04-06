ff3255320e8a8af7b1e3e3fde5fb3407e5574633
pippo1980 <philip.kissme@gmail.com>
2014-04-06 10:53:30 +0800

    首页

--- app.js
+++ app.js
@@ -6,19 +6,24 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+app.locals({
+    title: '背单词',
+    author: 'pippo',
+    email: 'pippo1980.du@gmail.com'
+});
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('public', express.static(path.join(__dirname, 'public')));
+app.use('static', express.static(path.join(__dirname, 'static')));
+//app.use(express.favicon());
 
-app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-
+app.use(express.cookieParser('8156'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
 app.use(app.router);
 
 // development only
@@ -34,5 +39,5 @@ app.get('/users', user.list);
 
 var http = require('http');
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('WordTutor server listening on port ' + app.get('port'));
 });
