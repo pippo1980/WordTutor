d3797e44acc0b77eca07731460dab26ed3485ce1
pippo1980 <philip.kissme@gmail.com>
2014-04-06 21:21:57 +0800

    单词管理

--- routes/word_manage.js
+++ routes/word_manage.js
@@ -2,6 +2,8 @@ var wordService = require('../service/WordService');
 
 exports.start = function (app) {
     app.get('/manage/word', home);
+    app.get('/manage/word/:id', getById);
+    app.delete('/manage/word/:id', deleteById);
     app.post('/manage/word', persist);
 }
 
@@ -11,10 +13,25 @@ function home(req, res) {
     });
 }
 
+function getById(req, res) {
+    var id = req.params.id;
+    wordService.getById(id, function (success, info) {
+        res.send(info != null ? info : {});
+    })
+}
+
+function deleteById(req, res) {
+    var id = req.params.id;
+    wordService.deleteById(id, function (success) {
+        //res.redirect(301, "/manage/word")
+        res.send({});
+    })
+}
+
 function persist(req, res) {
     var word = req.body;
     //console.log(word)
     wordService.persist(word, function (success, info) {
-        res.redirect("/manage/word")
+        res.redirect(301, "/manage/word")
     });
 }
